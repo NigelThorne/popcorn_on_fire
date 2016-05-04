72b6d952744cd6adaba05cd469f76ddd5862790f
Nigel Thorne <Nigel.Thorne@leicabiosystems.com>
2016-05-04 17:05:25 +1000

    master: Firebase popcord board

--- Main.elm
+++ Main.elm
@@ -4,8 +4,8 @@ import Dict exposing (Dict)
 import ElmFire
 import ElmFire.Dict
 import ElmFire.Op
-import Html exposing (Attribute, Html, button, div, input, li, ol, text)
-import Html.Attributes exposing (placeholder)
+import Html exposing (Attribute, Html, button, div, input, li, ol, text, span)
+import Html.Attributes exposing (name, value, type', placeholder)
 import Html.Events exposing (on, onClick, targetValue)
 import Json.Decode
 import Json.Encode
@@ -15,8 +15,7 @@ import Task exposing (Task)
 
 
 url =
-    "https://highscore-example.firebaseio.com/"
-
+    "https://popcornflow.firebaseio.com/"
 
 database =
     { location = ElmFire.fromUrl url
@@ -25,33 +24,48 @@ database =
     , decoder = Json.Decode.int
     }
 
+type alias Name = String
+type alias ProblemId = Int
+type alias ExperimentId = Int
+type alias OptionId = Int
+type alias Finding = String
+type alias Conclusion = String
+type alias Description = String
+type alias Expectation = String
+type alias Observation = String
+type alias Observer = String
 
 type Action
     = NoOp
-    | Dict (Dict String Int)
-    | Name String
-    | Score Int
-    | SubmitAndFetch
-
+    | NewProblem Name ProblemId
+    --| IncreasePriority ProblemId
+    --| NewOption ProblemId OptionId
+    --| NewExperiment OptionId ExperimentId
+    --| CommitToExperiment ExperimentId
+    --| StartExperiment ExperimentId
+    --| AddFindingsToExperiment ExperimentId Finding
+    --| EndExperiment ExperimentId
+    --| ConcludeExperiment ExperimentId Conclusion
 
 actions : Mailbox Action
 actions =
     Signal.mailbox NoOp
 
+--fetchDict : Task ElmFire.Error ()
+--fetchDict =
+--    ElmFire.Dict.getDict database `Task.andThen` (Dict >> Signal.send actions.address)
 
-fetchDict : Task ElmFire.Error ()
-fetchDict =
-    ElmFire.Dict.getDict database `Task.andThen` (Dict >> Signal.send actions.address)
-
-
-adjustDict : String -> Int -> Task ElmFire.Error ElmFire.Reference
-adjustDict name score =
-    ElmFire.Op.operate database (ElmFire.Op.update name (Just << Maybe.withDefault score << Maybe.map (max score)))
-
+--adjustDict : String -> Int -> Task ElmFire.Error ElmFire.Reference
+--adjustDict name score =
+--    ElmFire.Op.operate database (
+--        ElmFire.Op.update name (
+--            Just << Maybe.withDefault score << Maybe.map (max score)))
 
 type alias Model =
-    { dict : Dict String Int, name : String, score : Int }
+    { state : Dict String Int, nextProblemId : ProblemId }
 
+type alias Problem =
+    { id : ProblemId, name : Name, description : Description, observer: Observer }
 
 update : Action -> ( Model, x ) -> ( Model, Maybe (Task ElmFire.Error ()) )
 update action ( model, _ ) =
@@ -59,17 +73,17 @@ update action ( model, _ ) =
         NoOp ->
             ( model, Nothing )
 
-        Dict dict ->
-            ( { model | dict = dict }, Nothing )
+        --Dict dict ->
+        --    ( { model | state = dict }, Nothing )
 
-        Name name ->
-            ( { model | name = name }, Nothing )
+        --Name name ->
+        --    ( { model | name = name }, Nothing )
 
-        Score score ->
-            ( { model | score = score }, Nothing )
+        --Score score ->
+        --    ( { model | score = score }, Nothing )
 
-        SubmitAndFetch ->
-            ( model, Just (adjustDict model.name model.score `Task.andThen` always fetchDict) )
+        --SubmitAndFetch ->
+        --    ( model, Just (adjustDict model.name model.score `Task.andThen` always fetchDict) )
 
 
 onInput : Address a -> (String -> a) -> Attribute
@@ -78,14 +92,57 @@ onInput addr fun =
 
 
 view : Model -> Html
-view { dict } =
+view { model } =
     div
         []
-        [ input [ placeholder "Name", onInput actions.address Name ] []
-        , input [ placeholder "Score", onInput actions.address (Score << Result.withDefault 0 << String.toInt) ] []
-        , button [ onClick actions.address SubmitAndFetch ] [ text "Submit and fetch" ]
-        , ol [] (List.map (\pair -> li [] [ text (toString pair) ]) (List.reverse (List.sortBy snd (Dict.toList dict))))
+        [ viewProblemForm model
+        , div [] (List.map (\pair -> viewProblem (snd pair) ) (Dict.toList model))
         ]
+
+viewProblem : Problem -> Html
+viewProblem problem = 
+    div 
+        []
+        [ span [ type' "hidden", name "ProblemId"] [ text problem.id ]
+        , span [ placeholder "Name", name "Name"] [ text problem.name ]
+        , span [ placeholder "Description", name "Description"] [ text problem.description ]
+        , span [ placeholder "Observer", name "Observer"] [ text problem.observer ] 
+        ] 
+
+viewProblemForm : Model -> Html
+viewProblemForm model = 
+    div 
+        []
+        [ input [ type' "hidden", name "ProblemId", value model.nextProblemId ] []
+        , input [ placeholder "Name", name "Name" ] []
+        , input [ placeholder "Description", name "Description" ] []
+        , input [ placeholder "Observer", name "Observer" ] []
+        , button [ onClick actions.address NewProblem ] [ text "Add Problem" ]
+        ] 
+
+--viewOptionForm : Model -> Html
+--viewOptionForm = 
+--    div 
+--        []
+--        [ input [ type' "hidden", onInput actions.address ProblemId ] []
+--        , input [ type' "hidden", onInput actions.address OptionId ] []
+--        , input [ placeholder "Option", onInput actions.address Option ] []
+--        , input [ placeholder "Description", onInput actions.address Description ] []
+--        , button [ onClick actions.address NewOption ] [ text "Add Option" ]
+--        ] 
+
+--viewExperimentForm : Model -> Html
+--viewExperimentForm =
+--    div 
+--        []
+--        [ input [ type' "hidden", onInput actions.address OptionId ] []
+--        , input [ type' "hidden", onInput actions.address ExperimentId ] []
+--        , input [ placeholder "Experiment", onInput actions.address Experiment ] []
+--        , input [ placeholder "Description", onInput actions.address Description ] []
+--        , input [ placeholder "Expectation", onInput actions.address Expectation ] []
+--        , input [ placeholder "Observation", onInput actions.address Observation ] []
+--        , button [ onClick actions.address NewExperiment ] [ text "Add Experiment" ]
+--        ] 
 
 
 modelAndTasks : Signal ( Model, Maybe (Task ElmFire.Error ()) )
--- README.md
+++ README.md
@@ -6,3 +6,21 @@ An [Elm](http://elm-lang.org/) example showing how to use the [ThomasWeiser/elmf
 
 [Demo](https://jvoigtlaender.github.io/elm-highscore-example/Main.html)
 
+Popcorn Flow
+
+* Add new Observation 
+* Promote Observation to Problem
+* Add new Problem
+* Add option to Problem
+* Add Experiment (default state "uncommitted")
+	* Move experiment to committed state
+	* Move experiment to Ongoing state
+	* Move experiment to Review state
+	* Move experiment to Next state
+
+* Remove Observation 
+* Remove Problem => and all experiments, etc.
+* Remove Experiment 
+* Remove Observation
+
+
